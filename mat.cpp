4e0948afd4ac293f73aa0edf16daca93b7185bf5
shanimergui <69587799+shanimergui@users.noreply.github.com>
2022-03-09 11:15:11 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,33 @@
+#include "mat.hpp"
+#include <stdexcept>
+
+namespace ariel
+{
+    std::string mat(const int& num1,const int& num2 ,const char& sing1,const char& sing2 )
+    {
+        return " ";
+
+    }
+
+    std::string oddNumber(const int& num1,const int& num2)
+    {
+        int n;
+
+        for(int i=0; i<n;i++){
+                    
+        if (n%2==1)
+        {
+           return n; 
+        }
+        else return n-1;
+
+        }
+    }
+
+    std::string sing(const char& sing1 ,const char& sing2)
+    {
+        
+
+    }
+   
+} 
---
+++ mat.hpp
@@ -0,0 +1,10 @@
+#include <string>
+
+namespace ariel
+{
+    std::string mat(const int& num1,const int& num2, const char& sing1 ,const char& sing2);
+    std::string oddNumber(const int& num1,const int& num2);
+    std::string sing(const char& sing1 ,const char& sing2); 
+  
+} 
+ 
